b7341f734079700f7d7cb8182897cb44471b36a0
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-06-28 23:17:00 +0530

    [Harshitha] Calculating total Employee Wage for month

--- EmployeeWageCompution/Program.cs
+++ EmployeeWageCompution/Program.cs
@@ -7,29 +7,35 @@ namespace EmployeeWageCompution
         public const int FULL_TIME = 1;
         public const int PART_TIME = 2;
         public const int WAGE_PER_HR = 20;
+        public const int WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             Random random = new Random();
-            int empInput = random.Next(0, 3);
             int empHrs = 0;
             int empWage = 0;
-            switch(empInput)
+            int totalWage = 0;
+            int daysAbsent = 0;
+            for(int day = 1;day <= WORKING_DAYS;day++)
             {
-                case FULL_TIME:
-                    Console.WriteLine("Full time Employee");
-                    empHrs = 8;
-                    break;
-                case PART_TIME:
-                    Console.WriteLine("Part time Employee");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    break;
+                int empInput = random.Next(0, 3);
+                switch (empInput)
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        daysAbsent++;
+                        break;
 
+                }
+                empWage = empHrs * WAGE_PER_HR;
+                totalWage += empWage;
             }
-            empWage = empHrs * WAGE_PER_HR;
-            Console.WriteLine("Employee Wage per Day : "+empWage);
+            Console.WriteLine("Employee Wage for " + (WORKING_DAYS-daysAbsent) + " working days per month : " + totalWage);
+
         }
     }
 }
