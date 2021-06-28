8de49d9815da5eb33f04ff06f26619e0bb3f068d
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-06-28 23:49:14 +0530

    [Harshitha] Calculating wages by assigning max hrs as 100 and and max days as 20

--- EmployeeWageCompution/Program.cs
+++ EmployeeWageCompution/Program.cs
@@ -7,15 +7,17 @@ namespace EmployeeWageCompution
         public const int FULL_TIME = 1;
         public const int PART_TIME = 2;
         public const int WAGE_PER_HR = 20;
-        public const int WORKING_DAYS = 20;
+        public const int MAX_WORKING_DAYS = 20;
+        public const int MAX_WORKING_HRS = 100;
         static void Main(string[] args)
         {
             Random random = new Random();
             int empHrs = 0;
             int empWage = 0;
             int totalWage = 0;
-            int daysAbsent = 0;
-            for(int day = 1;day <= WORKING_DAYS;day++)
+            int day = 1;
+            int empWorkingHrs = 0;
+            while(day <= MAX_WORKING_DAYS && empWorkingHrs < MAX_WORKING_HRS)
             {
                 int empInput = random.Next(0, 3);
                 switch (empInput)
@@ -27,14 +29,17 @@ namespace EmployeeWageCompution
                         empHrs = 4;
                         break;
                     default:
-                        daysAbsent++;
+                        empHrs = 0;
                         break;
 
                 }
                 empWage = empHrs * WAGE_PER_HR;
+                empWorkingHrs += empHrs;
                 totalWage += empWage;
+                if (empInput != 0)
+                    day++;
             }
-            Console.WriteLine("Employee Wage for " + (WORKING_DAYS-daysAbsent) + " working days per month : " + totalWage);
+            Console.WriteLine("Employee Wage for " + day + " working days per month : " + totalWage);
 
         }
     }
