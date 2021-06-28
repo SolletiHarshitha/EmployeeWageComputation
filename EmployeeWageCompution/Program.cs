5d367ec6a0d1300865f168cb113ce95a32e5600a
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-06-28 21:20:41 +0530

    [Harshitha] Adding Employee Attendance File

---
+++ EmployeeWageCompution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWageCompution
+{
+    class Program
+    {
+        public const int FULL_TIME = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+            if(empInput==FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent"); ;
+            }
+        }
+    }
+}
