882a17c81c2560d8127a7d7f849af84e35e1403a
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-06-28 22:41:52 +0530

    [Harshitha] Adding Part time Employee and calculating wage

--- EmployeeWageCompution/Program.cs
+++ EmployeeWageCompution/Program.cs
@@ -5,17 +5,23 @@ namespace EmployeeWageCompution
     class Program
     {
         public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
         public const int WAGE_PER_HR = 20;
         static void Main(string[] args)
         {
             Random random = new Random();
-            int empInput = random.Next(0, 2);
+            int empInput = random.Next(0, 3);
             int empHrs = 0;
             int empWage = 0;
             if(empInput==FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Full Time Employee");
                 empHrs = 8;
+            }
+            else if(empInput==PART_TIME)
+            {
+                Console.WriteLine("Part Time Employee");
+                empHrs = 4;
             }
             else
             {
