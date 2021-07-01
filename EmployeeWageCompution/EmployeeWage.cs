086a7a033e5a6809b2fd5ec52c99fd293a2bee51
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-01 22:21:28 +0530

    [Harshitha] Computing Employee wage using class method

---
+++ EmployeeWageCompution/EmployeeWage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageCompution
+{
+    class EmployeeWage
+    {
+        public const int FULL_TIME = 1;
+        public const int PART_TIME = 2;
+        public const int WAGE_PER_HR = 20;
+        public const int MAX_WORKING_DAYS = 20;
+        public const int MAX_WORKING_HRS = 100;
+        public static void Compute()
+        {
+            Random random = new Random();
+            int empHrs = 0;
+            int empWage = 0;
+            int totalWage = 0;
+            int day = 1;
+            int empWorkingHrs = 0;
+            while (day <= MAX_WORKING_DAYS && empWorkingHrs < MAX_WORKING_HRS)
+            {
+                int empInput = random.Next(0, 3);
+                switch (empInput)
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+
+                }
+                empWage = empHrs * WAGE_PER_HR;
+                empWorkingHrs += empHrs;
+                totalWage += empWage;
+                if (empInput != 0)
+                    day++;
+            }
+            Console.WriteLine("Employee Wage for " + day + " working days per month : " + totalWage);
+        }
+    }
+}
